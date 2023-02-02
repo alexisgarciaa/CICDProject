b09376132668fa745b75d8be83142c20eaf3c802
bryan garcia <bryan.garcia@novatechdev.com>
2023-02-02 12:33:53 -0600

    Initial Commit

---
+++ CICDProject/CICDProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDProjectApp.swift
+//  CICDProject
+//
+//  Created by Alexis on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CICDProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
